ae9a33b334ef212d491b5da4bffca9d65be26807
AmreshRajvinderSingh <144646953+AmreshRajvinderSingh@users.noreply.github.com>
2025-04-19 20:17:23 -0400

    added backend base code

---
+++ email-writer-backend/src/test/java/com/email/writer_backend/EmailWriterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.writer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailWriterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
